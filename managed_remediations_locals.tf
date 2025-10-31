b683f0f3e1b959127bb3fb6aa74ddb6fe5c8c076
Matthew Schmitt <matt.schmitt@ent.security>
2025-10-31 15:15:59 -0700

    Adding remediation for vpc

---
+++ managed_remediations_locals.tf
@@ -0,0 +1,12 @@
+locals {
+  remediation_configurations = {
+    vpc-flow-logs-enabled = {
+      config_rule_name           = "vpc-flow-logs-enabled"
+      resource_type              = ["AWS::EC2::VPC"]
+      target_id                  = file("${path.module}/ssm-documents/enable_vpc_flow_logs.yaml")
+      target_type                = "SSM_DOCUMENT"
+      automatic                  = true
+      maximum_automatic_attempts = 3
+    }
+  }
+}
