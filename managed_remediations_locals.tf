0177791aad9c37ddf3bd9e2ddf3872e29fd58dfb
Matthew Schmitt <matt.schmitt@ent.security>
2025-11-04 09:26:13 -0800

    Updating SSM document name

--- managed_remediations_locals.tf
+++ managed_remediations_locals.tf
@@ -3,7 +3,7 @@ locals {
     vpc-flow-logs-enabled = {
       rule_name                  = "vpc-flow-logs-enabled"
       resource_type              = "AWS::EC2::VPC"
-      target_id                  = file("${path.module}/ssm_documents/enable_vpc_flow_logs.yaml")
+      target_id                  = "AWSSupport-EnableVPCFlowLogs"
       target_type                = "SSM_DOCUMENT"
       automatic                  = true
       maximum_automatic_attempts = 3
